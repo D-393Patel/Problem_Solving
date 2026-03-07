7f5da9acd9e4d48eb4ea5948c7b25a4fa2ac96be
D-393Patel <143238468+D-393Patel@users.noreply.github.com>
2026-03-07 10:31:01 +0530

    Time: 4 ms (46.77%), Space: 51.8 MB (80.51%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void dfs(int i,int j,char[][]grid,int[][]visited,int[][]dirs){
+        
+        int m=grid.length,n=grid[0].length;
+        if(i<0||j<0||i>=m||j>=n)
+        return;
+        if(grid[i][j]=='0' || visited[i][j]==1)
+        return;
+        visited[i][j]=1;
+        for(int[]d:dirs)
+        dfs(i+d[0],j+d[1],grid,visited,dirs);
+        
+    }
+    public int numIslands(char[][] grid) {
+    //to count the number of such islands
+    int count=0,m=grid.length,n=grid[0].length;
+    int [][]visited=new int[m][n];
+     int[][]dirs={{1,0},{0,1},{-1,0},{0,-1}};
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(grid[i][j]=='1' && visited[i][j]==0){
+            dfs(i,j,grid,visited,dirs);
+            count++;
+            }
+        }
+    }
+    return count;
+    }
+}
