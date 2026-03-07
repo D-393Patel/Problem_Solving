8984db64a5bf6960a1b9b6402e3a5fd2517a86db
D-393Patel <143238468+D-393Patel@users.noreply.github.com>
2026-03-07 10:45:16 +0530

    Time: 2 ms (99.87%), Space: 52.6 MB (20.96%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -20,16 +20,27 @@ class Solution {
     dfs1(i+1,j,grid,visited);
     dfs1(i,j+1,grid,visited);
     }
+    public void dfs2(int i,int j,char[][]grid){
+    if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]=='0')
+    return;
+    grid[i][j]='0';//once it is island so it can not be an island again so turn it into water.
+    dfs2(i-1,j,grid);
+    dfs2(i+1,j,grid);
+    dfs2(i,j-1,grid);
+    dfs2(i,j+1,grid);
+    }
     public int numIslands(char[][] grid) {
     //to count the number of such islands
     int count=0,m=grid.length,n=grid[0].length;
-    int [][]visited=new int[m][n];
-     int[][]dirs={{1,0},{0,1},{-1,0},{0,-1}};
+    // int [][]visited=new int[m][n];
+    //  int[][]dirs={{1,0},{0,1},{-1,0},{0,-1}};
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
-            if(grid[i][j]=='1' && visited[i][j]==0){
+            // if(grid[i][j]=='1' && visited[i][j]==0){
             // dfs(i,j,grid,visited,dirs);
-            dfs1(i,j,grid,visited);
+            // dfs1(i,j,grid,visited);
+            if(grid[i][j]=='1'){
+            dfs2(i,j,grid);
             count++;
             }
         }
